11547679f0bd77cb81a8eeb458ef93ccff64cf6a
tjrdls <tjrdls29@gmail.com>
2025-12-26 20:30:12 +0900

    init

--- bank_system/code/main.py
+++
@@ -1,8 +0,0 @@
-from bank_system.code import BankService
-
-bank = BankService()
-bank.create_account("alice", 1000)
-
-print("Deposit:", bank.deposit("alice", 200))
-print("Withdraw:", bank.withdraw("alice", 300))
-print("Withdraw too much:", bank.withdraw("alice", 2000))
